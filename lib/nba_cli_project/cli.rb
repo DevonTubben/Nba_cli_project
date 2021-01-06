5de1f5f20342115b7f91d6316d5cab9180694799
Devon Tubben <tubbend1@msu.edu>
2021-01-06 13:20:20 -0500

    started cli file

---
+++ lib/nba_cli_project/cli.rb
@@ -0,0 +1,33 @@
+#responsible for communicating between user and data
+
+class CLI 
+
+def start 
+    puts "Hello NBA fan! Let's start with your name:"
+
+    greet(user_input)
+end 
+
+def user_input 
+    gets.strip 
+end 
+
+def greet(name) 
+    puts "Welcome #{name}! Would you like to see a list of current NBA players?"
+    puts "Input 'Y' to see the list of NBA players or 'N' to exit!"
+end 
+
+def menu 
+    selection = user_input 
+
+    if selection == 'Y' 
+        # print the list of nba players
+
+    elsif selection == 'N' 
+        # give the user a goodbye message 
+
+    else 
+        # invalid output and make the user select again
+        
+    end 
+end 
