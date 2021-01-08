d116f2361c60c851892130f65c4a76be3a5322c8
Devon Tubben <tubbend1@msu.edu>
2021-01-08 14:33:53 -0500

    moved def line up

--- lib/nba_cli_project/api.rb
+++ lib/nba_cli_project/api.rb
@@ -1,7 +1,6 @@
 class API 
 
-
- def self.get_data
+def self.get_data
        response = RestClient.get('https://www.balldontlie.io/api/v1/players')
        player_array = JSON.parse(response)["data"]
        player_array.each do |player|
