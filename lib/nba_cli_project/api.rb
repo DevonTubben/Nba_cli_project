f4a4ebb430e45e65f679a7c7f9ba2f78016a88f9
Devon Tubben <tubbend1@msu.edu>
2021-01-08 14:27:18 -0500

    fixed error

--- lib/nba_cli_project/api.rb
+++ lib/nba_cli_project/api.rb
@@ -3,8 +3,8 @@ class API
 
  def self.get_data
        response = RestClient.get('https://www.balldontlie.io/api/v1/players')
-       player_arrays = JSON.parse(response)["data"]
-       player_arrays.each do |player|
+       player_array = JSON.parse(response)["data"]
+       player_array.each do |player|
         Players.new(player["first_name"], player["last_name"], player["team"], player["position"])
         Players.new(player)
         binding.pry
