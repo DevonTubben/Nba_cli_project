bac73e17c81f3ec5b26e464e2584247d281eeb5c
Devon Tubben <tubbend1@msu.edu>
2021-01-10 15:33:28 -0500

    updated cli. still not showing team name correct

--- lib/nba_cli_project/api.rb
+++ lib/nba_cli_project/api.rb
@@ -3,12 +3,18 @@ class API
 def self.get_data
        response = RestClient.get('https://www.balldontlie.io/api/v1/players')
        players_array = JSON.parse(response)["data"]
+       # binding.pry 
        players_array.each do |players|
-           Players.new(players)
-     binding.pry
-   end 
-   
+          Players.new(players)
+       
+       
 
- end
+          
+          # binding.pry 
+      end 
+  end
+
+
+
 
 end 
--- lib/nba_cli_project/cli.rb
+++ lib/nba_cli_project/cli.rb
@@ -1,73 +1,88 @@
 class CLI 
 
-def start 
-    puts "Welcome NBA fan! What name would you like us to call you by?"
-    API.get_data 
-    introduction(user_input)
-end 
-
-def user_input 
-    gets.strip 
-end 
-
-def introduction(name) 
-    puts "Awesome #{name}! Would you like to see a list of current NBA players?"
-    puts "Input 'y' to see the list of NBA players or 'n' to exit!"
-    menu 
-end 
-
-def player_list 
-    Players.all.each.with_index(1) do |players, i|
-        puts "#{i}, #{players.full_name}"
-end 
-player_choice
-end 
-
-
-def goodbye 
-    puts "Hopefully you found the information you were looking for. Bye!"
-end 
-
-def invalid 
-    puts "Invalid input. Please try again."
-    menu
-end 
-
-def player_choice  
-    puts "Choose a player to learn more information."
-
-    choice = user_input 
-    
-     players = Players.find_player(choice)
-     player_information(players)
-     menu
-end 
-
-def player_information(players)
-    puts "Name: #{players.full_name}"
-    puts "Position: #{players.position}"
-    puts "Height feet: #{players.height_feet}" 
-    puts "Height inches: #{players.height_inches}"
-    puts "Weight: #{players.weight_pounds}"
-    #Not all players will have their height or weight
-    puts "Team: #{players.team}"
-    
-end 
-
-
-
-def menu 
-    choice = user_input 
-
-    if choice == 'Y' || choice == 'y'
-        player_list
-        menu 
-    elsif choice == 'N' || choice == 'n'
-        goodbye 
-    else 
-        invalid
+    def start 
+        puts " "
+        puts "Welcome NBA fan:"
+        puts " " 
+        puts "What name would you like us to call you by?"
+        puts " "
+        API.get_data 
+        introduction(user_input)
     end 
-
-end 
-
-end 
+    
+    def user_input 
+        gets.strip 
+    end 
+    
+    def introduction(name) 
+        puts " "
+        puts "Awesome #{name}! Are you interested in learning about different player information: y or n"
+        puts " "
+        menu 
+    end 
+    
+    def player_list 
+        Players.all.each.with_index(1) do |players, i|
+            puts "#{i}, #{players.full_name}"
+    end 
+    player_choice
+    puts " " 
+    end 
+    
+    
+    def goodbye 
+        puts " "
+        puts "Thanks for stopping by. I hope we were able to help you find the information you were looking for."
+        puts " "
+        puts "Feel free to come back at anytime! "
+    end 
+    
+    def invalid 
+        puts " "
+        puts "Invalid input. Please try again."
+        puts " "
+    end 
+    
+    def player_choice  
+        puts " "
+        puts "Choose a player to learn more information:"
+        puts " "
+    
+        choice = user_input 
+        
+         players = Players.find_player(choice)
+         player_information(players)
+         menu
+    end 
+    
+    def player_information(players)
+        puts "Name: #{players.full_name}"
+        puts "Position: #{players.position}"
+        puts "Height feet: #{players.height_feet}" 
+        puts "Height inches: #{players.height_inches}"
+        puts "Weight: #{players.weight_pounds}"
+        #Not all players will have their height or weight
+        puts "Team: #{players.team}"
+        puts " "
+    end 
+    
+    
+    
+    def menu 
+        choice = user_input 
+    
+        if choice == 'Y' || choice == 'y'
+            puts " " 
+            puts "Here is the list of all NBA players:"
+            puts " "
+            player_list
+            menu 
+        elsif choice == 'N' || choice == 'n'
+            goodbye 
+        else 
+            invalid
+        end 
+    
+    end 
+    
+    end 
