fcacaf978c31f60d8d8012fe10f0c4427b626e03
Devon Tubben <tubbend1@msu.edu>
2021-01-10 16:29:19 -0500

    erased binding pry

--- lib/nba_cli_project/api.rb
+++ lib/nba_cli_project/api.rb
@@ -2,16 +2,11 @@ class API
 
 def self.get_data
        response = RestClient.get('https://www.balldontlie.io/api/v1/players')
-       players_array = JSON.parse(response)["data"]
-       # binding.pry 
+       players_array = JSON.parse(response)["data"] 
        players_array.each do |players|
           Players.new(players)
-       
-       
-
-          
-          # binding.pry 
       end 
+
   end
 
 
