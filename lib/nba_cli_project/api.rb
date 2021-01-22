eed10a28577a8129f6b4f1c85f1b69957068b846
Devon Tubben <tubbend1@msu.edu>
2021-01-21 19:03:42 -0500

    cleaned up

--- lib/nba_cli_project/api.rb
+++ lib/nba_cli_project/api.rb
@@ -3,8 +3,8 @@ class API
  def self.get_nba_players
       url = "https://www.balldontlie.io/api/v1/players?per_page=100"
       response = RestClient.get(url)
-      data = JSON.parse(response)["data"]
-      data.each do |players|
+      players_array = JSON.parse(response)["data"]
+      players_array.each do |players|
        Players.new(players)
     end 
   end 
